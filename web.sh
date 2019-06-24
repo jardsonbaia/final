074f33eeaec95e2bd641e5504c0acc3bfbfd4972
eduardaviriato <51841232+eduardaviriato@users.noreply.github.com>
2019-06-24 00:40:32 -0300

    Update web.sh

--- web.sh
+++ web.sh
@@ -19,12 +19,9 @@ sed -i 's/database_name_here/wordpress/g' wp-config.php
 sed -i 's/username_here/admin/g' wp-config.php
 sed -i 's/password_here/wordpress/g' wp-config.php
 sed -i "s/localhost/$IP_PRIVATE_BD/g" wp-config.php
-IP="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
 #echo $IP
 systemctl restart apache2.service
 rm -rf /var/www/html/index.html
-wp core install --url=http://$IP_PUBLIC --title=Blog\ Cozinha --admin_user=admin --admin_password=123456 --admin_email=teste@teste.com.br
-
 sed -i '/warn/a <Directory /var/www/html/>\n   AllowOverride All\n</Directory>' /etc/apache2/sites-available/000-default.conf
 a2enmod rewrite
 service apache2 restart
@@ -32,3 +29,6 @@ touch /var/www/html/.htaccess
 chown :www-data /var/www/html/.htaccess
 chmod 664 /var/www/html/.htaccess
 service apache2 restart
+P="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+#echo $IP
+wp core install --url=http://$P --title=Blog\ Cozinha --admin_user=admin --admin_password=123456 --admin_email=teste@teste.com.br
